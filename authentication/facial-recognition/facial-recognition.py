85a99640fc9a27b175ad132cda744b8dabf611da
Janie <emerald1901@gmail.com>
2019-07-03 16:21:49 +0700

    Add example images

--- authentication/facial-recognition/facial-recognition.py
+++ authentication/facial-recognition/facial-recognition.py
@@ -4,7 +4,6 @@
 # ==========================================================================
 
 import face_recognition as FR
-import cv2
 import numpy as np
 
 
@@ -13,44 +12,58 @@ class FaceRecogniser:
         self._names = []
         self._encodings = []
 
-    def encode_face(self, img_path):
-        img_file = FR.load_image_file(file=img_path)
-        face_encoding = FR.face_encodings(face_image=img_file)[0]
-        return face_encoding
-
-    def recognise_face(self, img_path):
-        image_file = FR.load_image_file(file=img_path)
-
+    @staticmethod
+    def encode_face(img_path):
+        """
+            Get encoding of the face in the given image
+        :param img_path: path to image of face
+        :return: encoding of face in image, if no face found, raise error and finish
+        """
         try:
-            # Find location of the face in image and get its encoding
-            face_encoding = FR.face_encodings(face_image=image_file)[0]
-
-            # Find face match from list of known faces
-            matches = FR.compare_faces(known_face_encodings=self._encodings,
-                                       face_encoding_to_check=face_encoding)
-
-            # If no face matches found, return 'Unknown'
-            if not True in matches:
-                return 'Unknown'
-
-            # If face matches are found
-            else:
-                # If only 1 match is found, return the person's name
-                if matches.count(True) == 1:
-                    name = self._names.index(matches.index(True))
-
-                # If more than 1 match is found, return the face with minimum distance
-                else:
-                    face_distances = FR.face_distance(face_encodings=self._encodings,
-                                                      face_to_compare=face_encoding)
-                    best_match_index = int(np.argmin(face_distances))
-                    name = self._names[best_match_index]
-
-                return name
-
+            img_file = FR.load_image_file(file=img_path)
+            face_encoding = FR.face_encodings(face_image=img_file)[0]
+            return face_encoding
         except IndexError:
             print('--FaceRecognitionErr: No face detected in given image.')
             quit()
 
+    def recognise_face(self, img_path):
+        """
+            Check if the person in image is within the member database
+            using face recognition
+        :param img_path: path to image of face
+        :return: name/user ID of the person if existing in database, else 'Unknown'
+        """
+        # Get face encoding from image
+        face_encoding = self.encode_face(img_path=img_path)
+
+        # Find face match from list of known faces
+        matches = FR.compare_faces(known_face_encodings=self._encodings,
+                                   face_encoding_to_check=face_encoding)
+
+        # If no face matches found, return 'Unknown'
+        if True not in matches:
+            return 'Unknown'
+
+        # If face matches are found
+        else:
+            # If only 1 match is found, return the person's name
+            if matches.count(True) == 1:
+                name = self._names[matches.index(True)]
+
+            # If more than 1 match is found, return the face with minimum distance
+            else:
+                face_distances = FR.face_distance(face_encodings=self._encodings,
+                                                  face_to_compare=face_encoding)
+                best_match_index = int(np.argmin(face_distances))
+                name = self._names[best_match_index]
+
+            return name
+
     def load_face_encodings(self):
         pass
+
+
+if __name__ == '__main__':
+    face_recogniser = FaceRecogniser()
+    print(face_recogniser.recognise_face(img_path='imgs/obama/obama2.jpg'))
