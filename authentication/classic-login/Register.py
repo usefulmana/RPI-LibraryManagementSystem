6c5166d4d53b26a3f0b3c9516e00b5d8a90e4996
quangminh1407 <minh14071996@>
2019-07-04 21:49:49 +0700

    another changes

--- authentication/classic-login/Register.py
+++ authentication/classic-login/Register.py
@@ -4,13 +4,16 @@ import bcrypt
 
 class Register(ValidateInput):
     def register(self):
-        username = self.validate_username()
+        email = self.validate_email()
         first_name = input("First Name: ")
         last_name = input("Last Name: ")
-        email = input("Email: ")
-        password = self.validate_password().encode("utf-8")
-        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-        self.curs.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s)", (username, first_name, last_name, email, hashed_password,))
-        self.conn.commit()
-        print("You've successfully registered")
-        self.conn.close()
+        password = self.validate_password()
+        while True:
+            retype_password = input("Retype your password: ")
+            if retype_password == password:
+                hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+                self.curs.execute("INSERT INTO library_users VALUES (%s, %s, %s, %s)", (email, first_name, last_name, hashed_password,))
+                self.conn.commit()
+                self.conn.close()
+                print("You've successfully registered")
+                break
