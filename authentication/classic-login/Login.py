ac0ba8ed032174552a3d62e32b67886462dd4357
quangminh1407 <minh14071996@>
2019-07-08 20:51:35 +0700

    adding socket_client

--- authentication/classic-login/Login.py
+++ authentication/classic-login/Login.py
@@ -1,5 +1,6 @@
 import bcrypt
 import MySQLdb
+import socket_client
 
 
 class Login:
@@ -9,18 +10,27 @@ class Login:
 
     def login(self):
         while True:
-            self.curs.execute("SELECT email, password FROM library_users")
+            self.curs.execute("SELECT email, password, status FROM library_users")
             data = self.curs.fetchall()
             email = input("Email: ")
             password = input("Password: ").encode("utf-8")
+            status = "logged in"
             for row in data:
                 if email == row[0]:
                     if bcrypt.checkpw(password, row[1].encode()):
-                        print("You've successfully logged in")
                         password = " "
-                        break
-            if password != " ":
-                print("Incorrect username or password")
-                continue
-            else:
+                    if row[2] != status:
+                        status = "logged out"
+                    break
+            if password == " " and status == "logged out":
+                print("You've successfully logged in")
+                self.curs.execute("UPDATE library_users SET status=%s WHERE email=%s", ("logged in", email))
+                self.conn.commit()
+                socket_client.socket_client(email)
                 break
+            elif password == " " and status == "logged in":
+                print("Someone is currently using this account")
+                break
+            elif password != " ":
+                print("Incorrect email or password")
+                continue
