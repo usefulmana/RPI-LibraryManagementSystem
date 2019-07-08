f094b69172b0fcea923eaf7aea50350f23588ef6
quangminh1407 <minh14071996@>
2019-07-08 20:51:35 +0700

    adding socket_client

--- authentication/classic-login/Register.py
+++ authentication/classic-login/Register.py
@@ -1,5 +1,5 @@
 from ValidateInput import ValidateInput
-import bcrypt
+import bcrypt, socket_client
 
 
 class Register(ValidateInput):
@@ -8,12 +8,14 @@ class Register(ValidateInput):
         first_name = input("First Name: ")
         last_name = input("Last Name: ")
         password = self.validate_password()
+        status = "logged in"
         while True:
             retype_password = input("Retype your password: ")
             if retype_password == password:
                 hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-                self.curs.execute("INSERT INTO library_users VALUES (%s, %s, %s, %s)", (email, first_name, last_name, hashed_password,))
+                self.curs.execute("INSERT INTO library_users VALUES (%s, %s, %s, %s, %s)", (email, first_name, last_name, hashed_password, status))
                 self.conn.commit()
                 self.conn.close()
                 print("You've successfully registered")
+                socket_client.socket_client(email)
                 break
