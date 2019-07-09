90338abffec3e38e65ad9e0d6557ab877f94bcd8
quangminh1407 <minh14071996@>
2019-07-09 11:42:01 +0700

    minor changes

--- authentication/classic-login/Main.py
+++ authentication/classic-login/Main.py
@@ -1,16 +1,20 @@
 from Login import Login
 from Register import Register
+import time
 
 
 class Main(Login, Register):
     def homepage(self):
+        print("Welcome to Library Management System")
         while True:
             option = input("Type L for Login, R for Register: ")
             if option == "L":
                 self.login()
+                time.sleep(3)
                 continue
-            elif option == "R":
+            if option == "R":
                 self.register()
+                time.sleep(3)
                 continue
             print("Wrong input. Please try again!")
 
